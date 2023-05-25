ec044534069a45cc32315f4768dce670954f2934
Nikita <crazefire@mail.ru>
2023-05-25 21:34:07 +0300

    fill

---
+++ Задача 54/DZ_54.cs
@@ -0,0 +1,66 @@
+﻿// // Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.WriteLine("");
+int rows = InputNumbers("Введите количество строчек: ");
+int columns = InputNumbers("Введите количество столбцов: ");
+int range = InputNumbers("Введите диапазон значений: от 1 до ");
+int[,] array = new int[rows, columns];
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void OrderArrayNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+Console.WriteLine("");
+Console.WriteLine($"Изначальный массив: ");
+Console.WriteLine("");
+CreateArray(array);
+PrintArray(array);
+Console.WriteLine("");
+Console.WriteLine($"Отсортированный массив: ");
+Console.WriteLine("");
+OrderArrayNumbers(array);
+PrintArray(array);
+Console.WriteLine("");
---
+++ Задача 56/DZ_56.cs
@@ -0,0 +1,66 @@
+﻿//Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("");
+int rows = InputNumbers("Введите количество строчек: ");
+int columns = InputNumbers("Введите количество столбцов: ");
+int range = InputNumbers("Введите диапазон значений: от 1 до ");
+int[,] array = new int[rows, columns];
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void RowSmallestSumOfNumbers(int[,] array)
+{
+    int counter = 1;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++, counter++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)    // Суммируем значение строк
+        {
+            result = result + array[i, j];
+        }
+        Console.Write($"Сумма чисел в {counter} строке - ({result})");
+        result = 0;
+        Console.WriteLine("");
+    }
+    
+}
+
+CreateArray(array);
+Console.WriteLine("");
+Console.WriteLine("Массив: ");
+Console.WriteLine("");
+PrintArray(array);
+Console.WriteLine("");
+Console.WriteLine("Рачет построчно: ");
+RowSmallestSumOfNumbers(array);
+Console.WriteLine("");
+Console.WriteLine($"Минимальная сумма в строке - ");
+
